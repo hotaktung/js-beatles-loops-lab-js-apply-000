b72606f392a7fa5c37053fcb8337544e713e35bc
Ho tak Tung <tunghotak@gmail.com>
2018-07-10 15:39:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // add solution here
+
+// For Loop Solution 
+function theBeatlesPlay(musicians, instruments) {
+var ss = [];
+var i;
+for (i=0; i<4 ; i++) {
+ ss[i] = musicians[i] + " plays " + instruments[i]
+}
+return ss
+}
+
+// While Loop solution 
+function johnLennonFacts(sxs) {
+var j = 0
+var a4 = sxs.length
+var FACTS = [];
+
+while (j<a4){
+  FACTS[j] = sxs[j] + "!!!"
+  j++;
+}
+return FACTS
+}
+
+// Do-while Loop
+function iLoveTheBeatles(sas) {
+var c = 7; var LUV =[]; var luv = [];
+  
+  do {
+    LUV[c-7] = "I love the Beatles!"
+    c++;
+  }
+  while (c < 15); 
+return LUV
+}
